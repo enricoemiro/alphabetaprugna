0f8efa5086d75ec9809edf2c6bba73dd88cf0c66
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2022-02-10 13:31:28 +0100

    Added docs to Point.java

--- src/mnkgame/AlphaBetaPrugna/Point.java
+++ src/mnkgame/AlphaBetaPrugna/Point.java
@@ -1,26 +1,36 @@
 package mnkgame.AlphaBetaPrugna;
 
 public class Point {
+  /** Abscissa */
   public int x;
+
+  /** Ordinate */
   public int y;
 
+  /** Point constructor. */
   public Point(int x, int y) {
     this.x = x;
     this.y = y;
   }
 
+  /** Point copy constructor. */
   public Point(Point point) {
     this.x = point.x;
     this.y = point.y;
   }
 
+  /**
+   * Multiply each coordinate by a scalar.
+   *
+   * @param scalar scalar value to multiply by
+   * @return current instance with updated coordinates
+   */
   public Point multiply(int scalar) {
     this.x *= scalar;
     this.y *= scalar;
     return this;
   }
 
-  public String toString() {
-    return String.format("(%d, %d)", x, y);
-  }
+  /** {@inheritDoc} */
+  public String toString() { return String.format("(%d, %d)", x, y); }
 }
