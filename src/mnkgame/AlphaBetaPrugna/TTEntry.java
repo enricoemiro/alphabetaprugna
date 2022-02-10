a29606f621538fa7fb9c82c43f969488e1568105
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2022-02-10 13:45:58 +0100

    Added docs in TTEntry.java

--- src/mnkgame/AlphaBetaPrugna/TTEntry.java
+++ src/mnkgame/AlphaBetaPrugna/TTEntry.java
@@ -1,27 +1,34 @@
 package mnkgame.AlphaBetaPrugna;
 
 public class TTEntry {
-  public static enum Flag { EXACT, ALPHA, BETA }
+  public static enum Flag {
+    /** means the value of the board was an EXACT score. */
+    EXACT,
+    /** means the value of the board was AT MOST equal to score. */
+    ALPHA,
+    /** means the value of the board was AT LEAST equal to score. */
+    BETA
+  }
 
+  /** Depth of the board configuration. */
   public int depth;
+
+  /** Score of the board configuration. */
   public int score;
 
-  /**
-   * Node Type:
-   * - Exact: means this is an exact score for the board.
-   * - Alpha: means the value of the node was at most equal to score.
-   * - Beta: means the value is at least equal to score.
-   */
+  /** Flag that describe the type of the score. */
   public Flag flag;
 
+  /** TTEntry default constructor. */
   public TTEntry() {
     this.depth = -1;
     this.score = -1;
     this.flag = null;
   }
 
+  /** {@inheritDoc} */
   public String toString() {
-    return String.format("depth: %d, score: %d, flag: %s",
-                         depth, score, flag.toString());
+    return String.format(
+            "depth: %d, score: %d, flag: %s", depth, score, flag.toString());
   }
 }
